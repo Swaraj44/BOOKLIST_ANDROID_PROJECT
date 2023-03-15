ef01c97724f6599b2782f37474a3afea204e64b6
Swaraj <swarajchbiswas44@gmail.com>
2023-03-16 05:14:59 +0600

    Booklist class created

---
+++ app/src/main/java/com/shakibcsekuet/mainbooklistproject/Book_List.java
@@ -0,0 +1,24 @@
+package com.shakibcsekuet.mainbooklistproject;
+
+
+
+public class Book_List{
+    
+    public  String book_name,book_id;
+
+    public String getBook_name() {
+        return book_name;
+    }
+
+    public void setBook_name(String book_name) {
+        this.book_name = book_name;
+    }
+
+    public String getBook_id() {
+        return book_id;
+    }
+
+    public void setBook_id(String book_id) {
+        this.book_id = book_id;
+    }
+}
