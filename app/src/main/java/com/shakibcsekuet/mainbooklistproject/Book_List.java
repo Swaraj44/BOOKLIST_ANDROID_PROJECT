f5f083ddee6f711f33a490ad7a019b2a6bfb96bd
Swaraj <swarajchbiswas44@gmail.com>
2023-03-16 06:31:50 +0600

    enjoy commiting

--- app/src/main/java/com/shakibcsekuet/mainbooklistproject/Book_List.java
+++ app/src/main/java/com/shakibcsekuet/mainbooklistproject/Book_List.java
@@ -5,7 +5,7 @@ package com.shakibcsekuet.mainbooklistproject;
 public class Book_List{
     
       public  String book_name;
-    int book_id;
+   
     
 
     public String getBook_name() {
@@ -16,12 +16,4 @@ public class Book_List{
         this.book_name = book_name;
     }
 
-    
-    public int getBook_id() {
-        return book_id;
-    }
-
-    public void setBook_id(int book_id) {
-        this.book_id = book_id;
-    }
 }
