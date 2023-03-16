efdc5659c625a637a84d44e31d3c234732bdae9a
Swaraj <swarajchbiswas44@gmail.com>
2023-03-16 06:28:24 +0600

    Retrying

---
+++ app/src/main/java/com/shakibcsekuet/mainbooklistproject/Book12.java
@@ -0,0 +1,25 @@
+package com.shakibcsekuet.mainbooklistproject;
+
+
+
+public class Book_List{
+    
+    public  String book22_name,book22_id;
+    
+
+    public String getBook22_name() {
+        return book22_name;
+    }
+
+    public void setBook22_name(String book22_name) {
+        this.book22_name = book22_name;
+    }
+
+    public String getBook22_id() {
+        return book22_id;
+    }
+
+    public void setBook22_id(String book22_id) {
+        this.book22_id = book22_id;
+    }
+}
