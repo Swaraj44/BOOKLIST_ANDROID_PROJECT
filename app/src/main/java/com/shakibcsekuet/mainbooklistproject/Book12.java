c7158c7639188a9f74195dbfef55d05174e0f803
Swaraj <swarajchbiswas44@gmail.com>
2023-03-16 06:36:30 +0600

    Trying to conflict again

---
+++ app/src/main/java/com/shakibcsekuet/mainbooklistproject/Book12.java
@@ -0,0 +1,26 @@
+package com.shakibcsekuet.mainbooklistproject;
+
+
+
+public class Book_List{
+    
+    public  String book22_name;
+    public int book22_id;
+    
+
+    public String getBook22_name() {
+        return book22_name;
+    }
+
+    public void setBook22_name(String book22_name) {
+        this.book22_name = book22_name;
+    }
+
+    public int getBook22_id() {
+        return book22_id;
+    }
+
+    public void setBook22_id(int book22_id) {
+        this.book22_id = book22_id;
+    }
+}
